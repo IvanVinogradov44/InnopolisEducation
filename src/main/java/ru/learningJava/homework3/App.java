2464d019b469310f3fb9bd0891f8a27906798619
vinogradov_ii <i.vinogradov.stc@innopolis.university>
2023-11-04 18:39:37 +0300

    сделал домашнее задание 5

--- src/main/java/ru/learningJava/homework3/App.java
+++ src/main/java/ru/learningJava/homework3/App.java
@@ -1,23 +1,94 @@
 package ru.learningJava.homework3;
 
-import java.util.Scanner;
+import java.io.ObjectInputStream;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
 
-        TV LG = new TV("LG",false,40.5);
-        Scanner scaner = new Scanner(System.in);
-        LG.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
-        LG.setScreenResolution("HD");
+        System.out.println("Введите значение максимального уровня громкости");
+        int maxVolume = -1;
+        Scanner scanMaxVolume = new Scanner(System.in);
+        while (maxVolume == -1) {
+            int maxVolumeInput = scanMaxVolume.nextInt();
+            if (maxVolumeInput < 100 && maxVolumeInput >0){
+                maxVolume = maxVolumeInput;
+                System.out.println("Максимальная громкость " + maxVolume);
+            }
+            else {
+                System.out.println("Некорректно указана максимальная громкость");
+            }
+        }
 
-        TV panasonic = new TV("Panasonic", true,108);
-        panasonic.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
-        panasonic.setScreenResolution("2K");
+        Random randomNumbers = new Random();
+
+        TV TV1 = new TV("LG",true,40);
+        TV1.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
+        TV1.setVolume();
+
+        TV TV2 = new TV("AOC", true,32);
+        TV2.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
+        TV2.setVolume(randomNumbers.nextInt(0, 100));
+
+        TV TV3 = new TV("Горизонт", true,55);
+        TV3.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
+        TV3.setVolume(randomNumbers.nextInt(0, 100));
+
+        TV TV4 = new TV("Sony", true,78);
+        TV4.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
+        TV4.setVolume(randomNumbers.nextInt(0, 100));
+
+        TV TV5 = new TV("Samsung", true,69);
+        TV5.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
+        TV5.setVolume(randomNumbers.nextInt(0, 100));
+
+        TV TV6 = new TV("Xiaomi", true,75);
+        TV6.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
+        TV6.setVolume(randomNumbers.nextInt(0, 100));
+
+        TV TV7 = new TV("Polarline", true,11);
+        TV7.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
+        TV7.setVolume(randomNumbers.nextInt(0, 100));
+
+        TV TV8 = new TV("Philips", false,9);
+        TV8.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
+        TV8.setVolume(randomNumbers.nextInt(0, 100));
+
+        TV TV9 = new TV("NanoCell", false,99);
+        TV9.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
+        TV9.setVolume(randomNumbers.nextInt(0, 100));
+
+        TV TV10 = new TV("ASUS", false,66);
+        TV10.setScreenFrequency(ScreenFrequncy.getRandomFrequncy());
+        TV10.setVolume(randomNumbers.nextInt(0, 100));
+
+        System.out.println("Максимальная громкость " + maxVolume);
+
+        Scanner scanTV = new Scanner(System.in);
+        TV[] TVList = new TV[10];
+        TVList[0] = TV1;
+        TVList[1] = TV2;
+        TVList[2] = TV3;
+        TVList[3] = TV4;
+        TVList[4] = TV5;
+        TVList[5] = TV6;
+        TVList[6] = TV7;
+        TVList[7] = TV8;
+        TVList[8] = TV9;
+        TVList[9] = TV10;
+
+        Arrays.sort(TVList);
+
+        System.out.println("Телевизоры  \n " +
+                "Включены\n" +
+                "с громкостью > " + maxVolume + "\n"+
+                "Отсортированы по возрастанию включенного канала\n");
+
+        for (int i = 0; i < TVList.length-1; i++)
+            if (TVList[i].getIsOn() && TVList[i].getVolume()<= maxVolume)
+                System.out.println(TVList[i]);
 
 
-        System.out.println(LG);
-        System.out.println();
-        System.out.println(panasonic);
 
     }
 }
--- src/main/java/ru/learningJava/homework3/TV.java
+++ src/main/java/ru/learningJava/homework3/TV.java
@@ -1,49 +1,100 @@
 package ru.learningJava.homework3;
 
+import java.util.Comparator;
+import java.util.Objects;
 import java.util.Random;
+import java.util.Scanner;
 
-public class TV {
+public class TV implements Comparable<TV>{
     private  String brandName;
     private int channelCount;
-    private boolean isColored;
-    private double diagonalSize;
     private String screenResolution;
-
-    public ScreenFrequncy getScreenFrequency() {
-        return screenFrequency;
-    }
-
-    public void setScreenFrequency(ScreenFrequncy screenFrequency) {
-        this.screenFrequency = screenFrequency;
-    }
-
     private ScreenFrequncy screenFrequency;
     private String weight;
+    private int currentChannel;
+    private int volume = -1;
+    private boolean isOn;
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    //Конструктор
+    public TV (String brandName, boolean isOn, int currentChannel){
+        this.brandName = brandName;
+        Random randomNumber = new Random();
+        this.weight = String.format("%.2f",randomNumber.nextDouble(5,15));
+        this.channelCount = 100;
+        this.isOn = isOn;
+        this.currentChannel = currentChannel;
+    }
+    public void setScreenFrequency(ScreenFrequncy screenFrequency) {this.screenFrequency = screenFrequency;}
 
     public void setScreenResolution(String screenResolution) {
         this.screenResolution = screenResolution;
     }
 
+    public void setVolume() {
+        System.out.println("Введите громкость " + this.brandName);
+        Scanner scanVolume = new Scanner(System.in);
+        while (volume == -1) {
+            int volumeValue = scanVolume.nextInt();
+            if (volumeValue > 100 || volumeValue < 0){
+                System.out.println("Некорректное значение громкости");
+            }
+            else {
+                this.volume = volumeValue;
+                System.out.println(this.brandName + " Громкость установлена " + this.volume);
+            }
+        }
+    }
 
-    public TV (String brandName, boolean isColored, double diagonalSize){
-        this.brandName = brandName;
-        this.isColored = isColored;
-        this.diagonalSize = diagonalSize;
-        Random randomNumber = new Random();
-        this.weight = String.format("%.2f",randomNumber.nextDouble(5,15));
-        this.channelCount = randomNumber.nextInt(10,100);
+    public void setVolume(int volume){
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public boolean getIsOn() {
+        return isOn;
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
     }
 
     @Override
     public String toString() {
-        return "TV{\n" +
-                "brandName='" + brandName + "\n" +
-                "channelCount=" + channelCount + "\n" +
-                "isColored=" + isColored + "\n" +
-                "diagonalSize=" + diagonalSize + "\n" +
-                "screenResolution='" + screenResolution + "\n" +
-                "screenFrequency='" + screenFrequency + "\n" +
-                "weight=" + weight +
+        return "TV{" +
+                "brandName='" + brandName + '\'' +
+                ", currentChannel='" + currentChannel + '\'' +
+                ", volume=" + volume +
+                ", isOn=" + isOn +
+                ", channelCount=" + channelCount +
+                ", screenResolution='" + screenResolution + '\'' +
+                ", screenFrequency=" + screenFrequency +
+                ", weight='" + weight + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TV tv = (TV) o;
+        return channelCount == tv.channelCount && volume == tv.volume && isOn == tv.isOn && Objects.equals(brandName, tv.brandName) && Objects.equals(screenResolution, tv.screenResolution) && screenFrequency == tv.screenFrequency && Objects.equals(weight, tv.weight) && Objects.equals(currentChannel, tv.currentChannel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brandName, channelCount, screenResolution, screenFrequency, weight, currentChannel, volume, isOn);
+    }
+
+    @Override
+    public int compareTo(TV compare) {
+        int value = ((TV) compare).currentChannel;
+        return this.currentChannel - value;
     }
 }
