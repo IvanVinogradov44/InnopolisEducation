9e5a5ac7fa4cc47081de214f9a213b659edefe67
Иван <i.vinogradov.stc@innopolis.university>
2023-12-03 17:28:26 +0300

    сделал домашку 10 обе задачи

---
+++ src/main/java/ru/learningJava/homework10/Sequence.java
@@ -0,0 +1,88 @@
+package ru.learningJava.homework10;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static java.lang.String.format;
+
+public class Sequence {
+    public static void main(String[] args) {
+        int[] intArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        System.out.println(Arrays.toString(intArray));
+        System.out.println(Arrays.toString(filter(intArray, new ByCondition() {
+            @Override
+            public boolean isOk(int number) {
+                if (number % 2 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        })));
+
+        System.out.println(Arrays.toString(filter(intArray, new ByCondition() {
+            @Override
+            public boolean isOk(int number) {
+                String currentNumber = "" + number;
+                int sumChars = 0;
+                for (int i = 0; i < currentNumber.length(); i++) {
+                    sumChars = sumChars + currentNumber.charAt(i);
+                }
+                if (sumChars % 2 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+
+            }
+        })));
+
+        Pair<Integer, String> pair = Pair.of(1, "hello");
+        Integer i = pair.getFirst(); // 1
+        String s = pair.getSecond(); // "hello"
+
+        Pair<Integer, String> pair2 = Pair.of(1, "hello");
+        boolean mustBeTrue = pair.equals(pair2); // true!
+        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
+
+        System.out.println(i);
+        System.out.println(s);
+        System.out.println(mustBeTrue);
+        System.out.println(mustAlsoBeTrue);
+    }
+
+    public static Integer[] filter(int[] intArray, ByCondition condition) {
+        Integer[] filtredArrayBuf = new Integer[intArray.length];
+        int j = 0;
+        for (int i = 0; i < intArray.length; i++) {
+            if (condition.isOk(intArray[i])) {
+                filtredArrayBuf[j] = intArray[i];
+                j++;
+            }
+        }
+
+        Integer[] filtredArray = new Integer[j];
+        for (int i = 0; i < j; i++) {
+            filtredArray[i] = filtredArrayBuf[i];
+        }
+        return filtredArray;
+    }
+
+
+//    public static int[] sumCharEachNumbers(int[] intArray, ByCondition condition){
+//        int[] sumCharEachNumbersArray = new int[intArray.length];
+//        String numberFromArray;
+//        for (int i = 0; i < intArray.length; i++) {
+//            numberFromArray = "" + intArray[i];
+//            int sumChars = 0;
+//            for (int j = 0; j <numberFromArray.length() ; j++) {
+//                sumChars = sumChars + Character.getNumericValue(numberFromArray.charAt(j));
+//                if (condition.isOk(intArray[i])) {
+//                    sumCharEachNumbersArray[i] = intArray[i];
+//                }
+//            }
+//        }
+//        return sumCharEachNumbersArray;
+//    }
+}
