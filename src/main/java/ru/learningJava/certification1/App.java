a2e0c5005c91ea69e55c5b5d1bf9ee03404e3cc2
Иван <i.vinogradov.stc@innopolis.university>
2024-01-18 01:11:30 +0300

    задание аттестации

--- src/main/java/ru/learningJava/certification1/App.java
+++ src/main/java/ru/learningJava/certification1/App.java
@@ -1,4 +1,17 @@
 package ru.learningJava.certification1;
 
+import com.github.javafaker.Faker;
+import ru.learningJava.certification1.model.User;
+import ru.learningJava.certification1.repositories.UserRepositoryFileImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
+    public static void main(String[] args) {
+        UserRepositoryFileImpl userRepositoryFile = new UserRepositoryFileImpl();
+        User newUser = new User("12","789ghs","789ghs","Иванов","Иван","Иванович",30,true);
+        userRepositoryFile.create(newUser);
+        System.out.println(newUser);
+    }
 }
--- src/main/java/ru/learningJava/certification1/repositories/UserRepositoryFileImpl.java
+++ src/main/java/ru/learningJava/certification1/repositories/UserRepositoryFileImpl.java
@@ -1,4 +1,51 @@
 package ru.learningJava.certification1.repositories;
 
-public class UserRepositoryFileImpl {
+import ru.learningJava.certification1.model.User;
+import ru.learningJava.certification1.utils.FileReaderWriter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepositoryFileImpl implements UserRepository{
+
+    List<User> userList = findAll();
+
+    @Override
+    public void create(User user) {
+        userList.add(user);
+        FileReaderWriter.writeUsersToFile(userList);
+    }
+
+    @Override
+    public User findById(String id) {
+
+        User foundUser = userList.stream().filter(user -> user.getId().equals(id)).findFirst().get();
+        return foundUser;
+    }
+
+    @Override
+    public List<User> findAll() {
+
+        return userList = FileReaderWriter.getUsers();
+    }
+
+    @Override
+    public void update(User updatedUser) {
+        User userForUpdate = userList.stream().filter(user -> user.getId().equals(updatedUser.getId())).findFirst().get();
+        userList.set(userList.indexOf(userForUpdate),updatedUser);
+        FileReaderWriter.writeUsersToFile(userList);
+    }
+
+    @Override
+    public void deleteById(String idForDelete) {
+        User userToDelete = userList.stream().filter(user -> user.getId().equals(idForDelete)).findFirst().get();
+        userList.remove(userToDelete);
+        FileReaderWriter.writeUsersToFile(userList);
+    }
+
+    @Override
+    public void deleteAll() {
+        userList.clear();
+        FileReaderWriter.deleteAll();
+    }
 }
